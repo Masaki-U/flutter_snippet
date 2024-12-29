641e51e24fce41e78aaa44783b23457af3f2cd77
kooo5252 <kooo5252@gmail.com>
2024-12-30 01:25:30 +0900

    add AppState sample

---
+++ lib/state_management/app_state/example.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_snippet/state_management/app_state/app_state.dart';
+import 'package:go_router/go_router.dart';
+import 'dart:math';
+import 'dart:convert';
+part 'example.g.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
+  }
+}
+
+// Define the routes using TypedGoRoute and AppStateRoute
+final _router = GoRouter(
+  routes: $appRoutes,
+);
+
+@TypedGoRoute<HomeRoute>(
+  path: '/',
+)
+class HomeRoute extends AppStateRoute {
+  const HomeRoute() : super(null);
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    _router.routerDelegate.currentConfiguration;
+    // Create or retrieve the ValueNotifier for the HomePage
+    final counterNotifier = AppState.instance.get<int>(
+      state,
+          () => ValueNotifier<int>(0),
+    );
+    return HomePage(counterNotifier: counterNotifier);
+  }
+}
+
+@TypedGoRoute<DetailRoute>(
+  path: '/detail/:id',
+)
+class DetailRoute extends AppStateRoute {
+  const DetailRoute(this.id) : super(id);
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    // Create or retrieve the ValueNotifier for the DetailPage
+    final counterNotifier = AppState.instance.get<int>(
+      state,
+          () => ValueNotifier<int>(0),
+        key: id,
+    );
+    return DetailPage(counterNotifier: counterNotifier);
+  }
+}
+
+// HomePage widget, receiving only ValueNotifier
+class HomePage extends StatelessWidget {
+  final ValueNotifier<int> counterNotifier; // Holds the ValueNotifier for the route
+
+  const HomePage({super.key, required this.counterNotifier});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')), // App bar title
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display the current counter value
+            ValueListenableBuilder<int>(
+              valueListenable: counterNotifier,
+              builder: (context, value, child) => Text('Counter: $value'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                counterNotifier.value++; // Increment the counter
+              },
+              child: const Text('Increment Counter'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                const DetailRoute("dsfzv").push(context);
+              },
+              child: const Text('Go to Detail Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// DetailPage widget, receiving only ValueNotifier
+class DetailPage extends StatelessWidget {
+  final ValueNotifier<int> counterNotifier; // Holds the ValueNotifier for the route
+
+  const DetailPage({super.key, required this.counterNotifier});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detail Page')), // App bar title
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Display the current counter value for the detail page
+            ValueListenableBuilder<int>(
+              valueListenable: counterNotifier,
+              builder: (context, value, child) => Text('Detail Counter: $value'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                counterNotifier.value++; // Increment the counter
+              },
+              child: const Text('Increment Detail Counter'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                DetailRoute(getRandString()).push(context);
+              },
+              child: const Text('Go to Detail Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String getRandString() {
+  var random = Random.secure();
+  var values = List<int>.generate(500, (i) =>  random.nextInt(255));
+  return base64UrlEncode(values);
+}
---
+++ lib/state_management/app_state/example.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $homeRoute,
+      $detailRoute,
+    ];
+
+RouteBase get $homeRoute => GoRouteData.$route(
+      path: '/',
+      factory: $HomeRouteExtension._fromState,
+    );
+
+extension $HomeRouteExtension on HomeRoute {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $detailRoute => GoRouteData.$route(
+      path: '/detail/:id',
+      factory: $DetailRouteExtension._fromState,
+    );
+
+extension $DetailRouteExtension on DetailRoute {
+  static DetailRoute _fromState(GoRouterState state) => DetailRoute(
+        state.pathParameters['id']!,
+      );
+
+  String get location => GoRouteData.$location(
+        '/detail/${Uri.encodeComponent(id)}',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
