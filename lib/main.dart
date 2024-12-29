01a2111d69896b77e674b6e6c4950880ab21ebd1
kooo5252 <kooo5252@gmail.com>
2024-12-30 04:53:27 +0900

    exampleはルートに移す

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_snippet/state_management/app_state/example.dart';
+import 'package:flutter_snippet/example.dart';
+import 'package:flutter_snippet/state_management/async/result.dart';
 
 void main() {
+  Result.success(345)
+      .map((value) async => "dsfdg$value")
+      .map((value) async => "dsfdg$value");
   runApp(const MyApp());
 }
